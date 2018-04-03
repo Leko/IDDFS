96cb2e3c38d8c01f0407fd04ae461e923246f255
Leko <leko.noor@gmail.com>
2018-04-04 00:41:27 +0900

    :white_check_mark: Add onVisit tests

--- test/simple-graph.js
+++ test/simple-graph.js
@@ -1,7 +1,9 @@
 // @flow
 import assert from 'assert'
 import { describe, it } from 'mocha'
+import sinon from 'sinon'
 import iddfs from '../src/index'
+import { type NodeId } from '../src/Strategy'
 
 describe('Simple graph', () => {
   const A = 'A'
@@ -31,5 +33,29 @@ describe('Simple graph', () => {
     })
 
     assert.equal(found, E)
+  })
+  it('Must call onVisit every visited nodes', async () => {
+    const spy = sinon.spy()
+    const expected = [
+      ['A', 0, []],
+      ['A', 0, []],
+      ['B', 1, ['B']],
+      ['C', 1, ['C']],
+      ['A', 0, []],
+      ['B', 1, ['B']],
+      ['A', 2, ['B', 'A']],
+      ['E', 2, ['B', 'E']],
+    ]
+
+    await iddfs({
+      initialNode: A,
+      isGoal: (node: Node) => node === E,
+      expand: (node: Node) => edges[node],
+      extractId: (node: Node) => node,
+      maxDepth: 3,
+      onVisit: spy,
+    })
+
+    assert.deepEqual(spy.getCalls().map(c => c.args), expected)
   })
 })
