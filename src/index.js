2b8d03d6a6b1ff733962b3f94ef3944e5c4cb615
Shingo Inoue <leko.noor@gmail.com>
2018-07-28 17:49:13 +0900

    Add getPath to find shortest path (#26)
    * :sparkles: Support expand returns Promise
    * :sparkles: Add getPath to retrieve shortest path
    * :memo: Add document about getPath

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ type Option<T> = {
   maxDepth?: number
 } & StrategyOptions<T>;
 
+type Result<T> = {
+  node: T,
+  path: Array<NodeId>
+};
+
 const defaults = {
   initialDepth: 0,
   maxDepth: Infinity
@@ -17,17 +22,17 @@ async function dls<T>(
   strategy: Strategy<T>,
   depthLimit: number,
   visited: Array<NodeId> = []
-): Promise<?T> {
+): Promise<?Result<T>> {
   const depth = visited.length;
   strategy.onVisit(node, depth, visited);
   if (depth === depthLimit) {
-    return strategy.isGoal(node) ? node : null;
+    return strategy.isGoal(node) ? { node, path: visited } : null;
   }
   if (!strategy.shouldContinue(node, depth, depthLimit)) {
     return null;
   }
 
-  const nodes: Array<T> = strategy.expand(node);
+  const nodes: Array<T> = await strategy.expand(node);
   for (let child of nodes) {
     const id: NodeId = strategy.extractId(child);
     const visitedDepth = strategy.isVisited(child, visited);
@@ -35,28 +40,41 @@ async function dls<T>(
       continue;
     }
 
-    const node = await dls(child, strategy, depthLimit, visited.concat([id]));
-    if (node !== null) {
-      return node;
-    }
-  }
-
-  return null;
-}
-
-export default async function search<T>(op: Option<T>): Promise<?T> {
-  const { initialDepth, maxDepth, initialNode, ...strategyOptions } = {
-    ...defaults,
-    ...op
-  };
-  const strategy = new Strategy((strategyOptions: StrategyOptions<T>));
-
-  for (let depthLimit = initialDepth; depthLimit <= maxDepth; depthLimit++) {
-    const found = await dls(initialNode, strategy, depthLimit);
+    const found = await dls(child, strategy, depthLimit, visited.concat([id]));
     if (found !== null) {
       return found;
     }
   }
 
   return null;
+}
+
+export async function search<T>(op: Option<T>): Promise<?Result<T>> {
+  const { initialDepth, maxDepth, initialNode, ...strategyOptions } = {
+    ...defaults,
+    ...op
+  };
+  const strategy = new Strategy((strategyOptions: StrategyOptions<T>));
+  const initialNodeId = strategy.extractId(initialNode);
+
+  for (let depthLimit = initialDepth; depthLimit <= maxDepth; depthLimit++) {
+    const found = await dls(initialNode, strategy, depthLimit + 1, [
+      initialNodeId
+    ]);
+    if (found !== null) {
+      return found;
+    }
+  }
+
+  return null;
+}
+
+export default async function getNode<T>(op: Option<T>): Promise<?T> {
+  const result = await search(op);
+  return result && result.node;
+}
+
+export async function getPath<T>(op: Option<T>): Promise<?Array<NodeId>> {
+  const result = await search(op);
+  return result && result.path;
 }
